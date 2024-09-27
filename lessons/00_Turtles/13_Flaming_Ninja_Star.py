7b399e6605276785b685fb08ac86af437b7eb9c1
evansheinberg <evans@saintjamesacademy.com>
2024-09-27 19:12:45 +0000

    SJA 5th Class

--- lessons/00_Turtles/13_Flaming_Ninja_Star.py
+++ lessons/00_Turtles/13_Flaming_Ninja_Star.py
@@ -13,7 +13,7 @@ def getRandomColor():
     return "#%06X" % (random.randint(0, 0xFFFFFF))
 
 
-colors = ["red", "blue", "green", "yellow", "orange"]
+colors = ["blue", "red", "white", "red", "white"]
 
 
 def getNextColor(i):
@@ -34,9 +34,9 @@ t.width(2)
 t.speed(0) 
 
 for i in range(25):
-    t.pencolor(getRandomColor())
+    t.pencolor("blue")()
 
-    t.fillcolor(getRandomColor()) 
+    t.fillcolor("red")
    
     t.begin_fill()
 
--- lessons/00_Turtles/15_Pentagon_Crazy.py
+++ lessons/00_Turtles/15_Pentagon_Crazy.py
@@ -1,41 +1,88 @@
-"""
-Pentagon Crazy
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup (width=600, height=600)    # Set the size of the window
 
-This program already works. Run it, then change it to make it draw a different pattern.
-"""
+tina = turtle.Turtle()                  # Create a turtle named tina
 
-import random
+tina.shape('turtle')                    # Set the shape of the turtle to a turtle
+tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
+
+
+colors = [ 'red', 'blue', 'black', 'orange']    # define a list of colors
+
+for color in colors:                            # loop through the colors
+    ... # Your code here
 import turtle
+#get the instance of turtle
+t=turtle.Turtle()
+#select color
+t.color('#4285F4','#4285F4') ## RBG value of color
+#change the pen size
+t.pensize(5)
+#change the drawing speed
+t.speed(1)
+## first circle for red color
+t.forward(120)
+t.right(90)
+t.circle(-150,50)  
+t.color('#0F9D58')
+t.circle(-150,100)
+t.color('#F4B400')
+t.circle(-150,60)
+t.color('#DB4437','#DB4437')
+t.begin_fill()
+t.circle(-150,100)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.circle(100,100)
+t.right(90)
+t.forward(50)
+t.end_fill()
+t.begin_fill()
+## second circle for yellow color
+t.color("#F4B400","#F4B400")
+t.right(180)
+t.forward(50)
+t.right(90)
+t.circle(100,60)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.circle(-150,60)
+t.end_fill()
+# third circle of green color
+t.right(90)
+t.forward(50)
+t.right(90)
+t.circle(100,60)
+t.color('#0F9D58','#0F9D58')
+t.begin_fill()
+t.circle(100,100)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.circle(-150,100)
+t.right(90)
+t.forward(50)
+t.end_fill()
+##Draw last circle
+t.right(90)
+t.circle(100,100)
+t.color('#4285F4','#4285F4')
+t.begin_fill()
+t.circle(100,25)
+t.left(115)
+t.forward(65)
+t.right(90)
+t.forward(42)
+t.right(90)
+t.forward(124)
+t.right(90)
+t.circle(-150,50)
+t.right(90)
+t.forward(50)
 
-def getRandomColor():
-    return "#%06X" % (random.randint(0, 0xFFFFFF))
-
-def getNextColor(i):
-    return colors[i % len(colors)]
-
-window = turtle.Screen()
-window.bgcolor("black")
-window.setup(width=600, height=600, startx=0, starty=0)
-
-colors = ("red", "blue", "green", "yellow", "orange")
-
-myTurtle = turtle.Turtle()
-myTurtle.shape("turtle")
-myTurtle.speed(0)
-myTurtle.width(1)
-
-sides = 5
-angle = 360 / sides
-
-for i in range(360):
-    if i == 100:
-        myTurtle.width(2)
-    if i == 200:
-        myTurtle.width(3)
-    myTurtle.pencolor(getNextColor(i))
-    myTurtle.forward(i)
-    myTurtle.right(angle + 1)
-
-myTurtle.hideturtle()
-
+#end
+t.end_fill()
+t.penup()
 turtle.done()
